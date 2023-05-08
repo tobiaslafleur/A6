3d196dda8f6b98029b794888b948d25bb3babe2d
Tobias la Fleur <tobias.lafleur98@gmail.com>
2023-05-08 16:28:22 +0200

    Created MovieForm component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+"use client";
+
+import MovieForm from "@/components/MovieForm";
+
 export default function Home() {
-  return <></>;
+  const handleSubmit = () => {};
+
+  return (
+    <div className="container mx-auto">
+      <MovieForm handleSubmit={handleSubmit} />
+    </div>
+  );
 }
--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,7 +1,40 @@
-export default function MovieForm() {
+export default function MovieForm({ handleSubmit }) {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="flex flex-col w-[40%]">
+        <div className="flex flex-col">
+          <label className="text-lg font-medium" form="title">
+            Titel:
+          </label>
+          <input
+            className="border my-2 p-2"
+            placeholder="Titel här..."
+            type="text"
+            id="title"
+            name="title"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-lg font-medium">Betyg:</label>
+          <select type="text" className="border my-2 p-2">
+            <option value="0" disabled selected>
+              Välj betyg här...
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+
+        <button
+          className="rounded-sm bg-green-600 text-white py-2 mt-2 w-[20%]"
+          type="submit"
+        >
+          Spara film
+        </button>
+      </form>
+    </div>
   );
 }
